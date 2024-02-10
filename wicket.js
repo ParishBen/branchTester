10d792bb67f514516eb3f681fc778c41ae222248
Benjamin Parish <parishben13@gmail.com>
2024-02-10 01:55:23 -0500

    new change

--- wicket.js
+++ wicket.js
@@ -1,6 +1,8 @@
 function tester1(param){
     let dasCount=0;
     for(let i = 0;i<param.length-3;i++){
-        dasCount+=param[i];
+        dasCount+=param[i]+1;
+        let chkpt = dasCount-2;
+        console.log("br2");
     }
 }
